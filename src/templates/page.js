eba9ebe484bf499af2a8b3bd13abc21138e031e0
Nora Humpage <nora.humpage@gmail.com>
2022-06-13 16:47:13 -0400

    add conditional featured image to page template and graphQL query

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,14 +1,25 @@
 import * as React from "react"
 import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+
 import Layout from "../components/Layout"
 import Seo from "../components/Seo"
+import * as styles from "./page.module.css"
 
 const Page = ({ data }) => {
   const page = data.wpPage
   return (
     <Layout>
-      <Seo title={page.title} image="/logo.png" pathname={page.uri} />
-      <article>
+      <Seo title={page.title} image="/logo.png" pathname={page.uri} article />
+      <article className={styles.article}>
+        {page.featuredImage && (
+          <figure>
+            <GatsbyImage
+              image={getImage(page.featuredImage.node.localFile)}
+              alt={page.featuredImage.node.altText}
+            />
+          </figure>
+        )}
         <h1>{page.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: page.content }} />
       </article>
@@ -24,6 +35,22 @@ export const query = graphql`
       title
       content
       uri
+      featuredImage {
+        node {
+          fileSize
+          mediaItemUrl
+          altText
+          localFile {
+            childImageSharp {
+              gatsbyImageData(
+                width: 1360
+                formats: [AUTO, WEBP, AVIF]
+                placeholder: BLURRED
+              )
+            }
+          }
+        }
+      }
     }
   }
 `
